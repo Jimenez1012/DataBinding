cc1e5f2f03754f32c7a48e51370e554b29d1cee0
Luis Enrique Jimenez <23470152@campeche.tecnm.mx>
2025-10-10 15:41:19 -0600

    ejemplo de Contador Manual usando controladores de eventos y un objeto de datos

---
+++ DataBinding.DataObject/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using DataBinding.DataObject.Models;
+
+namespace DataBinding.DataObject
+{
+    public partial class MainPage : ContentPage
+    {
+        private Contador contador;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            contador = new Contador();
+            ConteoLabel.Text = contador._Conteo.ToString();
+        }
+
+        private void OnContarButton_Clicked(object sender, EventArgs e)
+        {
+            contador._Contar();
+            ConteoLabel.Text = contador._Conteo.ToString();
+        }
+
+        private void OnReiniciarButton_Clicked(object sender, EventArgs e)
+        {
+            contador.Reiniciar();
+            ConteoLabel.Text = contador._Conteo.ToString();
+        }
+    }
+}
---
+++ DataBinding.DataObject/Models/Contador.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataBinding.DataObject.Models
+{
+    public class Contador
+    {
+        private int conteo;
+        public int _Conteo
+        {
+            get => conteo;
+            set
+            {
+                if (conteo != value)
+                {
+                    conteo = value;
+                }
+            }
+        }
+        public Contador()
+        {
+            _Conteo = 0;
+        }
+        public void _Contar()
+        {
+            _Conteo++;
+        }
+        public void Reiniciar()
+        {
+            _Conteo = 0;
+        }
+
+    }
+}
