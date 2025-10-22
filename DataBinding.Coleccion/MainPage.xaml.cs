2aecd8e8704a3f0c743e3d2d94482af3eccdd722
Luis Enrique Jimenez Pinzon <23570152@campeche.tecnm.mx>
2025-10-22 01:09:06 -0600

    Enlace de datos con un Collection

---
+++ DataBinding.Coleccion/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using DataBinding.Coleccion.Models;
+using System.Collections.ObjectModel;
+
+namespace DataBinding.Coleccion
+{
+    public partial class MainPage : ContentPage
+    {
+        private string _nombre = string.Empty;
+        private ObservableCollection<Usuario> _users;
+
+
+        public string Nombre
+        {
+            get => _nombre;
+            set
+            {
+                if (_nombre != value)
+                {
+                    _nombre = value;
+                    OnPropertyChanged(nameof(Nombre));
+                }
+            }
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+            _users = new ObservableCollection<Usuario>();
+            this.BindingContext = this;
+            this.UsuariosCollectionView.BindingContext = _users;
+        }
+
+        private void AgregarButtonClicked(object sender, EventArgs e)
+        {
+            AgregarUsuario(Nombre);
+
+        }
+
+        private void AgregarUsuario(string nombre)
+        {
+            if (!string.IsNullOrEmpty(nombre))
+            {
+                _users.Add(new Usuario(nombre));
+            }
+        }
+    }
+}
---
+++ DataBinding.Coleccion/Models/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBinding.Coleccion.Models
+{
+    public class Usuario
+    {
+        private string _nombre;
+
+        public string Nombre
+        {
+            get => _nombre;
+            set
+            {
+                if (_nombre != value)
+                {
+                    _nombre = value;
+                }                                    
+            }       
+        }
+
+        public Usuario(string nombre) 
+        { 
+            Nombre = nombre;
+        }
+    }
+}
